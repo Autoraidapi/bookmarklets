489a9da0b71fb93663402b4dd50258f4d7b5f610
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-09-23 20:56:33 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,3 +143,14 @@
     }
 }
 )([]);
+
+/*
+ * Media Source
+ */
+(function(xs){
+  for(var i = 0;i < xs.length;++i){
+    if(xs[i].currentSrc){
+      return window.open(xs[i].currentSrc);
+    }
+  }
+})(document.querySelectorAll('audio,video'));
