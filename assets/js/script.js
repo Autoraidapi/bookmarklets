19e3260935721174493f192d6f33c273142d42b1
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-09-23 20:55:01 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,23 @@
  * Dynamic Viewport
 */
 (function(meta){ 
-  
+	
+  function isNumber(obj) { 
+    return !!(obj === 0 || (obj && obj.toExponential && obj.toFixed)); 
+  }
+	
   if(document.getElementById('metaview')){ 
     document.head.removeChild(document.getElementById('metaview')); 
   }
-  
-  var io = prompt('Viewport Scale Between 0 and 1'), 
-  
+	
+  // var io = parseInt(prompt('Viewport Scale Between 0 and 1'));
+  var io = prompt('Viewport Scale Between 0 and 1');
+
+  if(!isNumber(io)){ 
+    alert('Requires a number'); 
+    return false; 
+  }
+	
   meta.id = 'metaview'; 
   meta.name = 'viewport'; 
   meta.content = 'width=device-width, initial-scale=' + io + ', minimum-scale='+io+', maximum-scale='+io+', user-scalable=no'; 
