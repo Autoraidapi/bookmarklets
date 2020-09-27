93b1475693ddd6410345e9ae2915b1cdd8ff3228
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-09-27 13:25:37 -0700

    Update
    Library folder added, bookmarklets expanded and debuggable

---
+++ assets/js/lib/media/open-media.js
@@ -0,0 +1,10 @@
+/*
+ * Media Source
+ */
+(function(xs){
+    for(var i = 0;i < xs.length;++i){
+      if(xs[i].currentSrc){
+        return window.open(xs[i].currentSrc);
+      }
+    }
+  })(document.querySelectorAll('audio,video'));
