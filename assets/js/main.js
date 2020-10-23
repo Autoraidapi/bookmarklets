99120c96565317d446fe5cfa68ea299506c77dbb
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-23 13:23:32 -0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,25 @@
-(function(){
-	
-	const Model = Backbone.Model.extend({
-		defaults : {
-			data : []
-		}
-	});
-	
-	const View = Backbone.View.extend({
-		el : $('#tablebody'),
-		template : _.template('\
-			<% _.each(this.model.get(\'data\'), function(obj, index, data) { %>\
-			   <tr><td><a href="<%= obj.uri %>"><%= obj.title %></a><p><%= obj.description %></p></td><td></td><td></td></tr>\
-			<% }); %>'
-		),
-		initialize : function(){		  	  
-			this.render();	
-		},
-		render : function(){
-		  this.$el.html(this.template());
-		  return this;
-		}
-	});
-	
-	function initialize(url){
-		const request = new XMLHttpRequest();
-		request.open('GET', url, true);
-		request.responseType = 'text';
-		request.onload = function(){
-			populate(request.response);
-		}
-		request.send(null);
-		function populate(json){
-			new View({
-				model : new Model({
-					data : JSON.parse(json).children
-				})
-			});
-		}
+const Model = Backbone.Model.extend();
+
+const Sync = Backbone.Collection.extend({
+	model : Model,
+	initialize : function(){ this.fetch(); }
+});
+
+const Developer = Sync.extend({ url : 'assets/json/developer.json' });
+const Link = Sync.extend({ url : 'assets/json/link.json' });
+const Layout = Sync.extend({ url : 'assets/json/layout.json' });
+const Media = Sync.extend({ url : 'assets/json/media.json' });
+
+
+const View = Backbone.View.extend({
+	template : _.template(
+		'<% _.each(function(){%>' +
+
+		'<% }) %>'
+	),
+	initialize : function(){},
+	render : function(){
+		this.template(this.colle.toJSON());
 	}
-
-	if(typeof window !== 'undefined'){
-		window.initialize = initialize;
-	}
-
-})();
-
+})
 
