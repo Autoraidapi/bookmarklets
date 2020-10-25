c4e70e7c230adcf4d222cd4eb86effc45c5e66bb
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-24 22:34:55 -0700

    Update open-media.js

--- scripts/media/open-media.js
+++ scripts/media/open-media.js
@@ -1,6 +1,6 @@
 /*
  * Media Source
- * window.open([selector][index].currentSrc);
+ * function (){ return window.open([selector][index].currentSrc); }
  */
 (function(xs){
     for(var i = 0;i < xs.length;++i){
