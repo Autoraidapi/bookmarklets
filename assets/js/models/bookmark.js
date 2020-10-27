9cd2c067c204ff3e3d7408b6b4770b8137caa36c
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-10-27 01:14:53 -0700

    Update bookmark.js

--- assets/js/models/bookmark.js
+++ assets/js/models/bookmark.js
@@ -1,11 +1,9 @@
 const Bookmark = Backbone.Model.extend({
-
     defaults: {
         name: '',
         title: '',
         uri: ''
     },
-
     validate: function () {
         var uri = this.get('uri');
         var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
@@ -17,11 +15,9 @@ const Bookmark = Backbone.Model.extend({
             console.log(JSON.stringify(this.toJSON()));  
         }
     },
-
     initialize: function () {      
       this.validate();      
     }
-
 });
 
 const github = new Bookmark({
