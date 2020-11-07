02b1488c3f914f8eab0f2c24f36792feeacea620
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-11-07 15:57:59 -0800

    Update router.js

--- assets/js/routes/router.js
+++ assets/js/routes/router.js
@@ -15,12 +15,15 @@ define(['jquery','underscore','backbone'], function($,_,Backbone) {
             Backbone.history.start();
         },
 
+        // setup registry keys, link keys to table rows, and table columns, return the data
         search : function(collection, model){
+            // key value filter
             console.log(collection, model);
         },
 
         filter : function(param){
             this.filter = param || '';
+            // testing, when done just return, string will be used a parameter to more functionality
             return console.log(this.filter);            
         }
 
