3dee18e86518a7f6ca1f76992d154b3806541f8e
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-04 02:00:34 -0800

    Update
    Redirect

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,17 @@ require.config({
 	shim: {
 		underscore: { exports: '_' },
 		backbone: { deps: [ 'underscore', 'jquery' ], exports: 'Backbone' }
-	},	
+	},
     paths: {
         jquery : 'https://assets.codepen.io/1674766/jquery.min',
         underscore : 'https://assets.codepen.io/1674766/underscore.min',
-        backbone : 'https://assets.codepen.io/1674766/backbone.min' 
+        backbone : 'https://assets.codepen.io/1674766/backbone.min', 
+        bootstrap : 'https://assets.codepen.io/1674766/bootstrap.bundle.min',
     }	
 });
 
-requirejs(['routes/router' ], function(Router){
+requirejs([ 'bootstrap', 'routes/router' ], function(Bootstrap, Router){
     if(typeof window !== 'undefined'){
-
-        window.app = new Router();        
-        
+        window.app = new Router();                
     }
 });
