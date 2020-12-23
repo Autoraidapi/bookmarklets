707d1c991c3724b8d9b50a79b8f227e4fdb058e8
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-22 21:34:45 -0800

    Update

--- assets/js/collections/bookmarks.js
+++ assets/js/collections/bookmarks.js
@@ -1,9 +1,26 @@
 define(['backbone','models/bookmark'], function (Backbone, Bookmark) {
 
-    const Bookmarks = Backbone.Collection.extend({
-
-        model : Bookmark
-
+    var Bookmarks = Backbone.Collection.extend({
+        model : Bookmark,
+        comparator : 'order',
+        export : function(){
+            
+            var json = JSON.stringify(this.toJSON(), null, 2);
+            var object = window.URL.createObjectURL(
+              new Blob(
+                [json], {type : 'application/octet-stream'}
+              )
+            );
+            
+            this.anchor = document.createElement('a');        
+            this.anchor.setAttribute('href', object);
+            this.anchor.setAttribute('download', prompt());    
+            this.anchor.click();
+            
+            URL.revokeObjectURL(object);
+            
+            delete this.anchor;
+        }
     });
 
     return Bookmarks;
