696afd2f0b2b548df5b83df26463fe1d3a556fc4
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-26 17:54:50 -0800

    Update container.js

--- assets/js/views/container.js
+++ assets/js/views/container.js
@@ -1,25 +1,37 @@
-define(['backbone', 'views/view'], function (Backbone) {
-
+define(['backbone', 'views/view'], function (Backbone, View) {
+	
+	var Header = Backbone.View.extend({
+		events : {
+			'click .btn' : 'handle'
+		},
+		handle : function(event){
+			
+		}
+	});
+	
 	var Container = Backbone.View.extend({
 		
-		el: $("#main"),
+		el: $(document.documentElement),
 		
 		initialize: function () {
-			this.$developer = new DeveloperView();
-			this.$links = new LinksView();
-			this.$media = new MediaView();
-			this.$layout = new LayoutView();
+			
+			this.Header = new Header();
+			
+			this.Developer = new View({ });
+			this.Links = new View();
+			this.Media = new View();
+			this.Layout = new View();
+			
 			this.listenTo(Bookmarklets, 'reset', this.refresh);
 			this.listenTo(Bookmarklets, 'all', _.debounce(this.render, 0));
 		},
 		
-		// render lists from the wrapping instance of this constructor
 		render : function(){
-		
+			return this;
 		},
 		
 		refresh : function(){
-		
+			
 		}
 
 	});
