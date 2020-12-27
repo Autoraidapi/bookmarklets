fd6656e5cdb88c1bb1a9dd1933c29433432c2e6b
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-26 17:57:40 -0800

    Update router.js

--- assets/js/routes/router.js
+++ assets/js/routes/router.js
@@ -1,9 +1,20 @@
 define(['backbone'], function(Backbone) {
-    var Router = Backbone.Router.extend({      
-        preintialize : function(){},
-        initialize : function(){
-            Backbone.history.start();
+    
+    var Router = Backbone.Router.extend({ 
+        
+        preinitialize : function(){},
+    		
+        routes : {        
+            'find(/:group)(/:id)' : 'find'          
+        },
+		
+        initialize : function(){}, 
+        
+        find : function(group, id){
+        
         }
+	 		
     });
+    
     return Router;
 });
