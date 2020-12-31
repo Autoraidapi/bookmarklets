d3333261761aef05f6fa82433a3cc111d3edf43c
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-31 15:10:13 -0800

    Update
    for a better view

--- assets/js/views/container.js
+++ assets/js/views/container.js
@@ -3,11 +3,15 @@ define(['backbone', 'views/view'], function (Backbone, View) {
 	var Container = Backbone.View.extend({
 		
 		el: $(document.documentElement),
+
+		// todo : render to fragment, then inject fragment to a cached dom node
+		fragment : new DocumentFragment(),
 		
 		initialize: function () {
 
 			this.listenTo(this.collection, 'add', this.addOne);
 			this.listenTo(this.collection, 'reset', this.addAll);
+			
 			this.listenTo(this.collection, 'all', _.debounce(this.render, 0));
 		},
 		
